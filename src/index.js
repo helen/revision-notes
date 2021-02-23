cede7f5d2993bdbab78f392763e0f3ebc8cf80e3
Helen Hou-Sandi <helen.y.hou@gmail.com>
2021-02-23 15:15:59 -0600

    Use state for the revision note display instead
    This is so that it doesn't initially load any message and also clears it after save.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
 import { TextareaControl } from '@wordpress/components';
+import { usePrevious } from '@wordpress/compose';
 import { dispatch, useSelect } from '@wordpress/data';
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
  
 const RevisionNotesField = () => {
-	const meta = useSelect((select) =>
-      select('core/editor').getEditedPostAttribute('meta'),
-    );
+	const [ revisionNote, setRevisionNote ] = useState('');
+
+	const { isSavingPost } = useSelect(
+		( select ) => ( {
+			isSavingPost:
+				select( 'core/editor' ).isSavingPost() &&
+				! select( 'core/editor' ).isAutosavingPost(),
+		} )
+	);
+	const prevIsSavingPost = usePrevious( isSavingPost );
+
+	useEffect( () => {
+		if (
+			! isSavingPost &&
+			prevIsSavingPost
+		) {
+			setRevisionNote('');
+		}
+	}, [ isSavingPost, prevIsSavingPost ] );
 
 	return (
 		<PluginPostStatusInfo>
 			<TextareaControl
 				label={__( 'Revision note (optional)', 'revision-notes' )}
 				help={__( 'Enter a brief note about this change', 'revision-notes' )}
-				value={meta.revision_note}
-				onChange={(value) =>
-					dispatch('core/editor').editPost({
-						meta: { revision_note: value },
-					})
+				value={revisionNote}
+				onChange={
+					(value) => {
+						setRevisionNote( value );
+
+						dispatch('core/editor').editPost({
+							meta: { revision_note: value },
+						});
+					}
 				}
 			/>
 		</PluginPostStatusInfo>
